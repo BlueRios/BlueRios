fef3f161f65ad5cf096eebeb3cdfcadbc56273b9
BlueRios <mazulrios@gmail.com>
2021-07-09 15:12:47 -0300

    Tp individual 2

---
+++ app.js
@@ -0,0 +1,15 @@
+const { SSL_OP_SINGLE_DH_USE } = require('constants');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/index.html')); 
+ });
+ 
+ app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(3008, () => {
+    console.log("Servidor corriendo");
+});
